de0a291498e6cc8cf68438c7da5950e53e452b64
jennyLiang214 <jennyl.xue@qq.com>
2017-06-16 11:39:49 +0800

    xiugai

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">jenny</div>
             </div>
         </div>
     </body>
